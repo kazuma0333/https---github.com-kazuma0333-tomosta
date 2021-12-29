f97a5914877e82dcc1ba416cbded3c99b74445fd
kazuma0333 <kazuma0333@gmail.com>
2021-12-29 21:20:08 +0900

    公開準備

---
+++ pages/about/index.js
@@ -0,0 +1,10 @@
+import Header from "../commponents/header"
+import Content from '../commponents/content'
+
+export default function About() {
+    return (
+        <Content>
+         <Header title="About" />
+        </Content>
+    )
+}
